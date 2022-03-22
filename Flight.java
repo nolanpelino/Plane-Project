e97cfc92806e710e67d16a5f602e815fe9885ac4
Matthew Kirk <matthewkirk@MacBook-Pro-126.local>
2022-03-22 12:55:01 -0400

    draft

---
+++ Flight.java
@@ -0,0 +1,26 @@
+import java.util.UUID;
+
+public class Flight {
+    private String planeModel;
+    private int flightlength;
+    private boolean hasTV;
+    private String departureAirport;
+    private String arrivalAirport;
+    private UUID id;
+
+    public Flight(String planeModel, int flightlength, boolean hasTV,
+    String depart, String arrival, UUID id) {
+        //TODO
+    }
+    public int getLength() {
+
+    }
+
+    public String getDeparture() {
+
+    }
+
+    public String getArrival() {
+
+    }
+}
---
+++ Hotel.java
@@ -0,0 +1,19 @@
+import java.util.UUID;
+
+public class Hotel {
+    private String address;
+    private String name;
+    private UUID id;
+
+    public Hotel(String address, String name, UUID id) {
+        //TODO
+    }
+
+    public String getName() {
+        return "";
+    }
+
+    public String getAddress() {
+        return "";
+    }
+}
