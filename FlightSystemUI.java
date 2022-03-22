c12e7fbde8d2457a495af2b6a3f8f0962c80b7d7
Matthew Maloney <mlm52@email.sc.edu>
2022-03-22 12:49:39 -0400

    flight UI and flight enums

---
+++ FlightSystemUI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FlightSystemUI {
+    Scanner scan = new Scanner(System.in);
+    private HotelList hotels;
+    private UserList users;
+    private FlightList flights;
+
+    public FlightSystemUI(){
+
+    }
+
+    public void run(){
+        
+    }
+}
