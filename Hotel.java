0a682680969072ab558ea0963e50fd2b59e1427a
Matthew Kirk <matthewkirk@MacBook-Pro-126.local>
2022-03-24 12:37:50 -0400

    Hotel specifications

--- Hotel.java
+++ Hotel.java
@@ -1,19 +1,37 @@
 import java.util.UUID;
 
 public class Hotel {
+    private Room[] rooms;
     private String address;
     private String name;
+    private int price;
     private UUID id;
+    private int stars;
+    private boolean pool;
 
-    public Hotel(String address, String name, UUID id) {
-        //TODO
+    public Hotel(Room[] roomarr, int price, String addr, String name, UUID ID,
+    int review, boolean pool) {
+        this.price = price;
+        this.address = addr;
+        this.name = name;
+        this.stars = review;
+        this.pool = pool;
     }
 
     public String getName() {
-        return "";
+        return this.name;
     }
 
     public String getAddress() {
-        return "";
+        return this.address;
+    }
+    public int getRating() {
+        return this.stars;
+    }
+    public boolean hasPool() {
+        return this.pool;
+    }
+    public void showOpenRooms(Date day) {
+        
     }
 }
