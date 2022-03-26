dfeea3869062c0af1eefade664514f071ce7a38f
Matthew Kirk <mdkirk@email.sc.edu>
2022-03-25 23:26:28 -0400

    Initial commit

---
+++ DataLoader.java
@@ -0,0 +1,9 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class DataLoader extends DataConstants {
+    
+}
