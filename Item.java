5784650ddbcab67e88d1e30c8ac54692bd84348c
Nolan Pelino <npelino@email.sc.edu>
2022-03-29 00:17:25 -0400

    Javadoc

--- Item.java
+++ Item.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 public abstract class Item {
     private double price;
 
+    /**
+     * 
+     * @return Price of an item as a double
+     */
     public double getPrice(){
         return this.price;
     }
