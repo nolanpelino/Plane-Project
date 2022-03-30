bceda9563c1f3cfdd8c22f3a54a41efb825771b3
Seth Dalmolin <sethdalmolin@Seths-MacBook-Pro.local>
2022-03-30 02:20:59 -0400

    Methods for adding to flights and hotels jsons

--- DataWriter.java
+++ DataWriter.java
@@ -38,4 +38,68 @@ public class DataWriter extends DataConstants {
 
         return userInfo;
     }
+
+    public static void saveFlights() {
+        FlightList flights = FlightList.getInstance();
+        ArrayList<Flight> fList = flights.getFlights();
+        JSONArray jsonFlights = new JSONArray();
+
+        for (int i = 0; i < fList.size(); i++) {
+            jsonFlights.add(getFlightJSON(fList.get(i)));
+        }
+
+        try (FileWriter file = new FileWriter(FLIGHTS_FILENAME)) {
+            file.write(jsonFlights.toJSONString());
+            file.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static JSONObject getFlightJSON(Flight flg) {
+        JSONObject flightInfo = new JSONObject();
+        flightInfo.put(FLIGHT_SEATS, flg.getOpenSeats());
+        flightInfo.put(FLIGHT_LENGTH, flg.getLength());
+        flightInfo.put(FLIGHT_HASTV, flg.hasTVs());
+        flightInfo.put(FLIGHT_DEPART, flg.getDeparture());
+        flightInfo.put(FLIGHT_ARRIVAL, flg.getArrival());
+        flightInfo.put(FLIGHT_ID, flg.getFlightNumber());
+        flightInfo.put(FLIGHT_NUMBER, flg.getFlightNumber());
+        flightInfo.put(FLIGHT_AIRLINE, flg.getAirline());
+
+        return flightInfo;
+    }
+
+    public static void saveHotel() {
+        HotelList hotels = HotelList.getInstance();
+        ArrayList<Hotel> hList = hotels.getHotels();
+        JSONArray jsonHotels = new JSONArray();
+
+        for (int i = 0; i < hList.size(); i++) {
+            jsonHotels.add(getHotelJSON(hList.get(i)));
+        }
+
+        try (FileWriter file = new FileWriter(HOTELS_FILENAME)) {
+            file.write(jsonHotels.toJSONString());
+            file.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static JSONObject getHotelJSON(Hotel htl) {
+        JSONObject hotelInfo = new JSONObject();
+        hotelInfo.put(HOTEL_ADDRESS, htl.getAddress());
+        hotelInfo.put(HOTEL_NAME, htl.getName());
+        hotelInfo.put(HOTEL_STARS, htl.getRating());
+        hotelInfo.put(HOTEL_PRICE, htl.getPrice());
+        hotelInfo.put(HOTEL_ID, htl.getID());
+        hotelInfo.put(HOTEL_ROOMS, htl.getRooms());
+        hotelInfo.put(HOTEL_HASPOOL, htl.hasPool());
+
+        return hotelInfo;
+    }
+
+
+
 }
