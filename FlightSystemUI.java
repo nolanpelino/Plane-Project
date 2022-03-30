1dcde6ee378610e0840cbdfea141f2503b815661
Nolan Pelino <npelino@email.sc.edu>
2022-03-29 22:25:07 -0400

    UI updates

--- FlightSystemUI.java
+++ FlightSystemUI.java
@@ -10,7 +10,9 @@ public class FlightSystemUI {
     }
 
     public void run(){
-        
+        int choice;
+        loginCheck();
+        choice = displayOptions();
     }
 
     
@@ -30,5 +32,33 @@ public class FlightSystemUI {
     public Hotel sortHotel(String address) {
 
     }
-    
+
+    public void loginCheck() {
+        String usname;
+        String password;
+        System.out.println("Welcome to the flight booking experience.\nEnter your Username: ");
+        usname = scan.next();
+        scan.nextLine();
+        // if statement to see if user exists
+        System.out.println("Enter your password: ");
+        password = scan.next();
+        scan.nextLine();
+        // if statement to decide if password is correct
+        System.out.println("Succesful login.");
+    }
+
+    public int displayOptions() {
+        boolean correct = false;
+        while (!correct) {
+            System.out.println("Choose your menu option :");
+            System.out.println("1- Book flight\n2- Book hotel\n3- View my flights\n4- View my hotel bookings\n5- View my family");
+            int choice = scan.nextInt();
+            if (choice < 1 || choice > 5){
+                System.out.println("Sorry that is not an option");
+            } else {
+                correct = true;
+            }
+        }
+        reutrn choice;
+    }
 }
