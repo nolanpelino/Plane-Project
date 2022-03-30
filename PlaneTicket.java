ed3e82dfabeb5a2c0a1a440e3cc62682b41520a5
Nolan Pelino <npelino@email.sc.edu>
2022-03-30 17:25:11 -0400

    Changed parameters

--- PlaneTicket.java
+++ PlaneTicket.java
@@ -1,23 +1,23 @@
 public class PlaneTicket {
     private Flight myFlight;
-    private User passenger;
     private Seat yourSeat;
     private String gate;
+    private String username;
+    private int age;
 
-    public PlaneTicket(Flight myFlight, User passenger, Seat yourSeat, String gate) {
+    public PlaneTicket(Flight myFlight, String username, int age, Seat yourSeat, String gate) {
         this.myFlight = myFlight;
-        this.passenger = passenger;
         this.yourSeat = yourSeat;
         this.gate = gate;
     }
 
     public void displayTicket() {
-        System.out.println(passenger.getUsername());
+        System.out.println(username);
         
         if(passenger.getAge() < 12)
             System.out.println("Child Passenger");
         else
-            System.out.println("Age " + passenger.getAge());
+            System.out.println("Age " + age);
 
         System.out.println("Flight " + myFlight.getFlightNumber());
         System.out.println("From " + myFlight.getDeparture() + " to " + myFlight.getArrival());
