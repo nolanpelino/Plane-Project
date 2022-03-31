d611091292d34f35b32fbdaa6e51d7782c73373c
Matthew Maloney <mlm52@email.sc.edu>
2022-03-31 18:00:02 -0400

    USERLIST should be finished!

--- UserList.java
+++ UserList.java
@@ -1,21 +1,27 @@
 import java.util.ArrayList;
 
 public class UserList {
-    private static UserList userlist;
-    private ArrayList<User> users;
+    private static UserList users;
+    private ArrayList<User> userL;
     
     private UserList() {
-        // TODO
+        userL = DataLoader.getUsers();
     }
 
     public static UserList getInstance() {
-        if(userlist == null) {
-            userlist = new UserList();
-            return userlist;
+        if(users == null) {
+            users = new UserList();
         }
-        else {
-            return userlist;
+            return users;
+    }
+    //Checks to see if user is already created retuns boolean
+    public boolean haveUser(String userName){
+        for(User user: userL){
+            if(user.getUsername().equals(userName)){
+                return true;
+            }
         }
+        return false;
     }
 
     /**
@@ -24,9 +30,9 @@ public class UserList {
      * @return An instance of a user that matches the passed in Username
      */
     public User getUser(String userName) {
-        for (User us : users) {
-            if(us.getUsername().equals(userName))
-                return us;
+        for (User user : userList) {
+            if(user.getUsername().equals(userName))
+                return user;
         }
         System.out.println("User not found");
         return null;
@@ -37,6 +43,16 @@ public class UserList {
      * @return the list of users
      */ 
     public ArrayList<User> getUsers() {
-		return users;
+		return userL;
 	}
+
+    public boolean addUser(String username, String password, int age, String address, boolean frequentFlyer, String homeAirport, boolean passport, boolean hasDisability) {
+    if(haveUser(username)) return false;
+    userL.add(new User(username, password, age, address, frequentFlyer, homeAirport, passport, hasDisability))
+    return true;
+    }
+
+    public void saveUsers(){
+        DataWriter.saveUsers();
+    }
 }
