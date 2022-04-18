ee69a4cbb3df9387552f2b0b4b16eeba3ae7a1b8
Nolan Pelino <npelino@email.sc.edu>
2022-04-18 17:59:43 -0400

    Added Testing for chooseroom

--- HotelTest.java
+++ HotelTest.java
@@ -11,14 +11,33 @@ public class HotelTest {
         //Tests with empty ArrayList
         ArrayList<Room> roomsList = new ArrayList<Room>();
         String id = "468676b4-9709-455e-941d-bc89e0846f7f";
-        Date today = new Date(System.currentTimeMillis());  //Current Time
+        Date today = new Date(System.currentTimeMillis());  // Current Time
         UUID hotelID = UUID.fromString(id);
 
         ArrayList<Date> stayDates = new ArrayList<Date>();
         stayDates.add(today);
-        
+
         Hotel tstHotel = new Hotel(roomsList, "address", "city", "state", "name",
          200, hotelID , 4, true);
         tstHotel.showOpenRooms(stayDates);
+    }
+
+    @Test
+    public void testChooseRoom() {
+        // Testing choose room for a time thats taken
+        ArrayList<Room> roomsList = new ArrayList<Room>();
+        Room rm = new Room(222, 200, 1);
+        roomsList.add(rm);
+        String id = "468676b4-9709-455e-941d-bc89e0846f7f";
+        Date today = new Date(System.currentTimeMillis());  // Current Time
+        UUID hotelID = UUID.fromString(id);
+
+        ArrayList<Date> stayDates = new ArrayList<Date>();
+        stayDates.add(today);
+        rm.setAvailability(today);  // Sets the room unavailable for the same time the user would be looking for
+
+        Hotel tstHotel = new Hotel(roomsList, "address", "city", "state", "name",
+         200, hotelID , 4, true);
+        tstHotel.chooseRoom(222, stayDates);
     }
 }
