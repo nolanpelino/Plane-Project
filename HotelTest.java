dbbd5b123fe24c28019b17c0a79f69cdf956f334
Nolan Pelino <npelino@email.sc.edu>
2022-04-18 17:55:20 -0400

    Test case changes

--- HotelTest.java
+++ HotelTest.java
@@ -8,11 +8,15 @@ public class HotelTest {
     
     @Test
     public void testEmptyShowOpenRooms() {
-        //Tests with empty ArrayList and No dates
+        //Tests with empty ArrayList
         ArrayList<Room> roomsList = new ArrayList<Room>();
         String id = "468676b4-9709-455e-941d-bc89e0846f7f";
+        Date today = new Date(System.currentTimeMillis());  //Current Time
         UUID hotelID = UUID.fromString(id);
+
         ArrayList<Date> stayDates = new ArrayList<Date>();
+        stayDates.add(today);
+        
         Hotel tstHotel = new Hotel(roomsList, "address", "city", "state", "name",
          200, hotelID , 4, true);
         tstHotel.showOpenRooms(stayDates);
