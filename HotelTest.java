a285295761dd57765780b2046e1a53f68e9782ac
Nolan Pelino <npelino@email.sc.edu>
2022-04-18 17:51:14 -0400

    Testing Setup

---
+++ HotelTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.UUID;
+
+
+public class HotelTest {
+    
+    @Test
+    public void testShowOpenRooms() {
+        //Tests with empty ArrayList and No dates
+        ArrayList<Room> roomsList = new ArrayList<Room>();
+        String id = "468676b4-9709-455e-941d-bc89e0846f7f";
+        UUID hotelID = UUID.fromString(id);
+        ArrayList<Date> stayDates = new ArrayList<Date>();
+        Hotel tstHotel = new Hotel(roomsList, "address", "city", "state", "name",
+         200, hotelID , 4, true);
+        tstHotel.showOpenRooms(stayDates);
+    }
+}
