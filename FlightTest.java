107e2c52718265c1ce3bb27a3cb8617865e8e04b
Matthew Maloney <mlm52@email.sc.edu>
2022-04-18 20:47:10 -0400

    Testing get open seats

--- FlightTest.java
+++ FlightTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
 import java.util.UUID;
 
 
@@ -10,14 +12,46 @@ public class FlightTest {
         Seat[] seats = new Seat[30]; 
         UUID testUUID = UUID.randomUUID();
         Seat testSeat = new Seat(200, "First Class", 1, 'B', true);
-        seats[0]=testSeat;
+        seats[0]=testSeat;//Sets the test seat to the first seat in the array
         Flight testFlight = new Flight(seats, 200, true, "CLT", "ATL", testUUID, 2209, "American", 0);
-        Seat finalTestedSeat = testFlight.takeSeat("1B");
+        Seat finalTestedSeat = testFlight.takeSeat("1B");//Attempts to book this seat (should return that seat)
         assertEquals(testSeat, finalTestedSeat);
     }
 
     @Test
     public void testTakeNotOpenSeat(){
+        Seat[] seats = new Seat[30]; 
+        UUID testUUID = UUID.randomUUID();
+        Seat testSeat = new Seat(200, "First Class", 1, 'B', true);
+        seats[0]=testSeat;
+        Flight testFlight = new Flight(seats, 200, true, "CLT", "ATL", testUUID, 2209, "American", 0);
+        Seat takenSeat = testFlight.takeSeat("1B");//Seat is taken and therefore should not be able to be booked
+        Seat tryingToTake = testFlight.takeSeat("1B");//Attempting to book the seat again (should return null)
+        assertEquals(null, tryingToTake);
+
+    }
+
+    @Test 
+    public void testGetOpenSeats(){
+        ArrayList<Seat> correctSeatArrayList = new ArrayList<>();
+        Seat[] seats = new Seat[30]; 
+        UUID testUUID = UUID.randomUUID();
+        Seat testSeat1 = new Seat(200, "First Class", 1, 'A', true);
+        Seat testSeat2 = new Seat(200, "First Class", 1, 'B', true);
+        Seat testSeat3 = new Seat(200, "First Class", 1, 'C', true);
+        seats[0]=testSeat1;
+        seats[1]=testSeat2;
+        seats[3]=testSeat3;
+        correctSeatArrayList.add(testSeat1);
+        correctSeatArrayList.add(testSeat2);
+        correctSeatArrayList.add(testSeat3);
+        Flight testFlight = new Flight(seats, 200, true, "CLT", "ATL", testUUID, 2209, "American", 0);
+        ArrayList<Seat> testSeatArrayList =testFlight.getOpenSeats();
+        assertEquals(correctSeatArrayList, testSeatArrayList);
+    }
+
+    @Test
+    public void testGetNoOpenSeats(){
 
     }
 }
