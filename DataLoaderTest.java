941d5ae0dc46d513ea281344bf6ee0fe2404c7ca
Nolan Pelino <npelino@email.sc.edu>
2022-04-20 16:35:18 -0400

    Change to test method

--- DataLoaderTest.java
+++ DataLoaderTest.java
@@ -13,17 +13,19 @@ public class DataLoaderTest {
     @Test
     public void testGetUser() throws IOException, ParseException {
         // Tetsing to see if getUser correctly obtains all the users from the user.json file
-        ArrayList<User> usList = new ArrayList<User>();
+        
         ArrayList<User> compList = new ArrayList<User>();
-        usList = DataLoader.getUsers();
+        UserList usList = UserList.getInstance();
+        // HARDCODING
         UUID uuid = UUID.fromString("7244f7ac-a242-417d-9aa5-6ad7fb5fde5b");
         UUID uuid2 = UUID.fromString("104207fd-5447-462b-9486-ad309a2e72bc");
         User us1 = new User("KarieMicheals20", "Applest0Bananas3", uuid, 35, "638 W University Boulevard\nOdessa, Texas 79964\nUnited States",
          true, "ODO", true, false);
         User us2 = new User("user", "pass", uuid2, 20, "638 W University Boulevard\nOdessa, Texas 79964\nUnited States",
         true, "ODO", true, false);
+        // HARDCODING
         compList.add(us1);
         compList.add(us2);
-        assertEquals(compList, usList);
+        assertEquals(compList, usList.getUsers());
     }
 }
