97bcf47511802c45aa19189caac697bdad8e2576
Nolan Pelino <npelino@email.sc.edu>
2022-04-20 16:50:09 -0400

    Added testGetUser()

--- UserListTest.java
+++ UserListTest.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 public class UserListTest {
    
     @Test
-    public void testGetUser() throws IOException, ParseException, org.json.simple.parser.ParseException {
+    public void testGetUsers() throws IOException, ParseException, org.json.simple.parser.ParseException {
         /* Testing to see if getUser correctly obtains all the users from the user.json file.
         getUser uses the dataloader within its class  */
         
@@ -27,5 +27,16 @@ public class UserListTest {
         compList.add(us1);
         compList.add(us2);
         assertEquals(compList, usList.getUsers());
+    }
+
+    public void testGetUser() throws IOException, org.json.simple.parser.ParseException {
+        // Testing to see if getUser returns the specific user we are looking for
+        UserList usList = UserList.getInstance();
+        UUID uuid = UUID.fromString("7244f7ac-a242-417d-9aa5-6ad7fb5fde5b");
+        User karieUser = new User("KarieMicheals20", "Applest0Bananas3", uuid, 35, "638 W University Boulevard\nOdessa, Texas 79964\nUnited States",
+         true, "ODO", true, false);
+
+        User returnedUser = usList.getUser("KarieMicheals20");
+        assertEquals(karieUser, returnedUser);
     }
 }
