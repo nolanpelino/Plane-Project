0f72cd33272c3511fb925d615a6752af0a6956cb
Nolan Pelino <npelino@email.sc.edu>
2022-04-20 16:31:22 -0400

    UUID Change

--- DataLoaderTest.java
+++ DataLoaderTest.java
@@ -17,7 +17,7 @@ public class DataLoaderTest {
         ArrayList<User> compList = new ArrayList<User>();
         usList = DataLoader.getUsers();
         UUID uuid = UUID.fromString("7244f7ac-a242-417d-9aa5-6ad7fb5fde5b");
-        UUID uuid2 = UUID.fromString("7244f7ac-a242-431p-9aa5-6ad7fb5fde5b");
+        UUID uuid2 = UUID.fromString("104207fd-5447-462b-9486-ad309a2e72bc");
         User us1 = new User("KarieMicheals20", "Applest0Bananas3", uuid, 35, "638 W University Boulevard\nOdessa, Texas 79964\nUnited States",
          true, "ODO", true, false);
         User us2 = new User("user", "pass", uuid2, 20, "638 W University Boulevard\nOdessa, Texas 79964\nUnited States",
